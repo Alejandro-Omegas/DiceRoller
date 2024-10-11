784b6def59b0db099db9613c4aa11e4800cd403e
Alejandro Arriola <arbaad@protonmail.com>
2024-10-10 23:30:25 -0300

    agregdp borde rojo

--- app/src/main/java/com/alejandroarriola/diceroller/MainActivity.kt
+++ app/src/main/java/com/alejandroarriola/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -18,9 +19,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.alejandroarriola.diceroller.ui.theme.DiceRollerTheme
 
@@ -65,6 +68,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 
         Button(
             onClick = { result = (1..6).random() },
+            modifier = Modifier.border(width = 2.dp, color = Color.Red)
         ) {
             Text(text = stringResource(R.string.roll), fontSize = 24.sp)
         }
